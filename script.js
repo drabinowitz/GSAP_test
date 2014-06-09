0a4edfc7d082bd154c60d620765c5c6152f4f3bd
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-06-09 18:08:26 -0400

    updates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 			$("#tester").click(function () {
 				$(this).hide();
 			});
-			var tester = $("#tester");
+			var tester = $(".tester");
 			tester.text("newer text");
 			TweenLite.to(tester,3,{left:"632px",ease:Quart.easeInOut,backgroundColor:"green"});
 		})
